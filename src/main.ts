7d4265044d179c6d47c0e354e0c03103357253c4
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2025-09-25 18:43:08 +0200

    Dont validate trades
    Signed-off-by: Jakub Dzikowski <jakub.t.dzikowski@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -21,12 +21,7 @@ async function main(): Promise<void> {
     const prices = await service.updatePrices();
     const stats = await service.calculateStats();
     const balances = await service.fetchBalances(prices);
-    const trades = await service.rebalance(balances, stats);
-
-    if (trades.length > 0) {
-      const newBalances = await service.fetchBalances(prices);
-      await service.saveTrades(balances, newBalances, trades);
-    }
+    await service.rebalance(balances, stats);
   } finally {
     await db?.disconnect();
   }
