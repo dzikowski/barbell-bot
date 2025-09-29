9b627bad38844415f80e5be40ceef038d50fe56f
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2025-09-29 14:10:19 +0200

    Make trades assymmetric
    Signed-off-by: Jakub Dzikowski <jakub.t.dzikowski@gmail.com>

--- src/tradingService.ts
+++ src/tradingService.ts
@@ -14,7 +14,7 @@ const targetPercentageOther = (100 - targetPercentageGala) / otherTokens.length;
 const tolerance = (type: "sell" | "buy") =>
   type === "sell"
     ? 0.05 // 5% for sell
-    : 0.05; // 5% for buy
+    : 0.08; // 5% for buy
 
 interface BalanceInfo {
   token: string;
